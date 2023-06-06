4094f9296a9395e11f1938e5a2c3cd293f1e2ec1
Pawan Singhania <pawansinghania001@gmail.com>
2023-06-07 00:23:28 +0530

    initial commit

---
+++ index.py
@@ -0,0 +1,31 @@
+from skpy import SkypeEventLoop, SkypeNewMessageEvent
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv('CHAT_GPT_KEY')
+print(openai.api_key)
+
+class SkypePing(SkypeEventLoop):
+    def __init__(self, username, password):
+        super(SkypePing, self).__init__(username, password)
+        print("initialized! watching for events")
+
+    def onEvent(self, event):
+        if isinstance(event, SkypeNewMessageEvent) \
+            and not event.msg.userId == self.userId :
+            print(event.msg)
+            # sending automated reply
+            response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                    {"role": "system", "content": "You're name is Pawan."},
+                    {"role": "user", "content": event.msg.content},
+                ]
+            )['choices'][0]['message']['content']
+            event.msg.chat.sendMsg(response)
+
+sk = SkypePing('pawansinghania001@gmail.com','Mybestemailid@001')
+sk.loop()
